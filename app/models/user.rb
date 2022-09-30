fc2c501037787f5923db35bf61678f8bfcc6d3a2
GlenCorreia <glen.correia@pubnub.com>
2022-09-30 11:31:47 +0530

    Remove name from table schema and add uuid generation method to user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
+require 'securerandom'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  # CONSTANTS
+
+  # FILTERS
+
+  # VALIDATORS
+
+  # ASSOCIATIONS
+
+  # SCOPES
+
+  # METHODS
+
+  before_create :add_uuid
+
+
+  private
+
+    def add_uuid
+      self.uuid = SecureRandom.uuid
+    end
+
 end
